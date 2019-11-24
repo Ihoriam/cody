b2935ab67ea1c657f8229d62a608d4099c059de6
Ihnatenko Ihor <igignt@gmail.com>
2019-11-24 21:50:59 +0200

    Make base html page and css style

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/main')
+def mainPage():
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
