ac84ef2a736a25e322a024019039ac69dc7851e3
Ihnatenko Ihor <igignt@gmail.com>
2020-01-12 19:48:37 +0200

    Made the first working version

--- checky/routes.py
+++ checky/routes.py
@@ -1,6 +1,6 @@
-from app import app
-from flask import render_template, flash, redirect, url_for, request
-from app.forms import CheckerForm
+from checky import app
+from flask import render_template, request
+from checky.forms import CheckerForm
 import subprocess
 import os
 
@@ -24,7 +24,6 @@ def mainPage():
                                  universal_newlines=True)
         output = process.stdout
         flag_valid = 1 if output.strip() == form.outputField.data.strip() else 0
-        print(flag_valid)
 
         return render_template('result.html',
                                code=form.codeField.data,
@@ -35,14 +34,3 @@ def mainPage():
                                )
     else:
         return render_template('main.html', form=form)
-
-
-# @app.route('/result', methods=['GET', 'POST'])
-# def resultPage():
-#
-#     return 'Result'
-
-if __name__ == '__main__':
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.run()
